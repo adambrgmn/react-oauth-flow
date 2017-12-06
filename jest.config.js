3bc134f52acdb7183d06c5d2bbb3922765926c45
Adam Bergman <adam@fransvilhelm.com>
2017-12-06 14:23:46 +0100

    test: Lower coverageThreshold
    Lower coverage threshold to a more reasonable level at the initial implementation of this project.
    More tests will probably come and raise the level.

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@ const jestConfig = require('frans-scripts/jest');
 jestConfig.setupFiles = jestConfig.setupFiles || [];
 jestConfig.setupFiles.push('<rootDir>/tests/setup.js');
 
+jestConfig.coverageThreshold = {
+  global: {
+    branches: 30,
+    functions: 30,
+    lines: 30,
+    statements: 30,
+  },
+};
+
 module.exports = jestConfig;
