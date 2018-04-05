3d8b924a8cd3499d76c185ab44e7068873e57415
Adam Bergman <adam@fransvilhelm.com>
2018-04-05 09:58:54 +0200

    fix: Remove use of User-Agent header
    And also remove appName prop since it was only used within User-Agent
    
    fix #19

--- src/OauthReceiver/index.js
+++ src/OauthReceiver/index.js
@@ -3,7 +3,6 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import qs from 'qs';
 import { buildURL, fetch2 } from '../utils';
-import pkg from '../../package.json';
 
 export class OauthReceiver extends React.Component {
   static propTypes = {
@@ -11,7 +10,6 @@ export class OauthReceiver extends React.Component {
     clientId: PropTypes.string.isRequired,
     clientSecret: PropTypes.string.isRequired,
     redirectUri: PropTypes.string.isRequired,
-    appName: PropTypes.string,
     args: PropTypes.objectOf(
       PropTypes.oneOfType([
         PropTypes.string,
@@ -33,7 +31,6 @@ export class OauthReceiver extends React.Component {
     args: {},
     location: null,
     querystring: null,
-    appName: pkg.name,
     onAuthSuccess: null,
     onAuthError: null,
     render: null,
@@ -58,7 +55,6 @@ export class OauthReceiver extends React.Component {
         clientId,
         clientSecret,
         redirectUri,
-        appName,
         args,
         onAuthSuccess,
       } = this.props;
@@ -82,10 +78,7 @@ export class OauthReceiver extends React.Component {
         ...args,
       });
 
-      const headers = new Headers({
-        'User-Agent': appName,
-        Accept: 'application/json',
-      });
+      const headers = new Headers({ 'Content-Type': 'application/json' });
 
       fetch2(url, { method: 'POST', headers })
         .then(response => {
