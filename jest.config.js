7458515f453079065a6c037d52697c16f780b365
Adam Bergman <adam@fransvilhelm.com>
2018-08-14 13:51:46 +0200

    chore: Install eslint and jest

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,11 @@
-const jestConfig = require('frans-scripts/jest');
-
-jestConfig.setupFiles = jestConfig.setupFiles || [];
-jestConfig.setupFiles.push('<rootDir>/tests/setup.js');
-
-jestConfig.coverageThreshold = {
-  global: {
-    branches: 30,
-    functions: 30,
-    lines: 30,
-    statements: 30,
+module.exports = {
+  setupFiles: ['<rootDir>/tests/setup.js'],
+  coverageThreshold: {
+    global: {
+      branches: 30,
+      functions: 30,
+      lines: 30,
+      statements: 30,
+    },
   },
 };
-
-module.exports = jestConfig;
