bc599edc51ebde1b7aa61238cbaff5b3543d82b3
Adam Bergman <adam@fransvilhelm.com>
2018-08-15 23:53:11 +0200

    test: Update components tests

--- src/OauthSender/OauthSender.test.js
+++ src/OauthSender/OauthSender.test.js
@@ -1,29 +1,30 @@
-/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, cleanup } from 'react-testing-library';
 import { OauthSender } from './index';
 import { buildURL } from '../utils';
+
+afterEach(cleanup);
 
 test('Component: <OauthSender />', () => {
   const props = {
     authorizeUrl: 'https://www.service.com/oauth2/authorize',
     clientId: 'abc',
     redirectUri: 'https://www.test.com/redirect',
-    render: (
-      { url }, // eslint-disable-line
-    ) => (
-      <a className="link" href={url}>
+    render: ({ url }) => (
+      <a data-testid="link" href={url}>
         Connect
       </a>
     ),
   };
 
-  const wrapper = shallow(<OauthSender {...props} />);
-  const expectedUrl = buildURL(`${props.authorizeUrl}`, {
+  const { getByTestId } = render(<OauthSender {...props} />);
+
+  const expectedUrl = buildURL(props.authorizeUrl, {
     client_id: props.clientId,
     redirect_uri: props.redirectUri,
     response_type: 'code',
   });
 
-  expect(wrapper.find('.link').prop('href')).toEqual(expectedUrl);
+  expect(getByTestId('link')).toHaveAttribute('href', expectedUrl);
 });
